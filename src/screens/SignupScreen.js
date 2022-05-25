09703bb1ff47f101682c2c3be05b18ba4158d6f0
Bhakti Daraniya <180540107035@darshan.ac.in>
2022-05-25 18:00:48 +0530

    automatic-signing-done

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,41 +1,29 @@
-import React, { useState, useContext } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
-import Spacer from '../components/Spacer';
+import React, { useContext, useEffect } from 'react';
+import { View, StyleSheet} from 'react-native';
 import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+import { NavigationEvents } from 'react-navigation';
 
 const SignupScreen = ({ navigation }) => {
-  const { state, signup } = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { state, signup , clearErrorMessage,tryLocalSignin} = useContext(AuthContext);
+
+  useEffect(() => {
+    tryLocalSignin();
+  }, [])
 
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        autoCorrect={false}
+      <NavigationEvents onWillBlur={() => { clearErrorMessage}}/>
+      <AuthForm
+        headerText="Sign Up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signup}
       />
-      <Spacer />
-      <Input
-        secureTextEntry
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
-      {state.errorMessage ? (
-        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-      ) : null}
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signup({ email, password })} />
-      </Spacer>
+      <NavLink 
+        routeName="Signin"
+        text="Already Have an account? Sign in instead"/>
     </View>
   );
 };
@@ -52,12 +40,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginBottom: 250,
   },
-  errorMessage: {
-    fontSize: 16,
-    color: 'red',
-    marginLeft: 15,
-    marginTop: 15,
-  },
+
 });
+
 
 export default SignupScreen;
